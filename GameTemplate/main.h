7c52ccc6a6b4620e68e97cca3582027daafb14ff
Graham Hill <49126644+GrahamCHill@users.noreply.github.com>
2024-12-31 22:26:55 +0200

    Initial File Commit of working SDL3 Vulkan Build with DearImGUI
    - Works on Apple
    - Should work on Windows & Linux

---
+++ GameTemplate/main.h
@@ -0,0 +1,19 @@
+//
+// Created by Graham Hill on 2024/12/31.
+//
+
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <iostream>
+#include <vulkan/vulkan.h>
+
+#include "imgui.h"
+#include "imgui_impl_sdl3.h"
+#include "imgui_impl_vulkan.h"
+#include <stdio.h>          // printf, fprintf
+#include <stdlib.h>         // abort
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_vulkan.h>
+
+#endif //MAIN_H
