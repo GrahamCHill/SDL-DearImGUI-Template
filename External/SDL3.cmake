10fc9b7507ad29c64e2dae38ebfa9af48b3ae3f8
Graham Hill <49126644+GrahamCHill@users.noreply.github.com>
2025-01-05 00:47:21 +0200

    Windows Support Fixed
    Should Reliably build for Windows platforms now

--- External/SDL3.cmake
+++ External/SDL3.cmake
@@ -1,17 +1,16 @@
 if(WIN32)
-    ## Set SDL3 directory based on the git submodule location
-    #set(SDL3_DIR ${CMAKE_CURRENT_SOURCE_DIR}/SDL3)
-    #message(STATUS "SDL3_DIR: ${SDL3_DIR}")
-    #
-    ## Add SDL3 as a subdirectory
-    #add_subdirectory(${SDL3_DIR})
+    # Set SDL3 directory based on the git submodule location
+    set(SDL3_DIR ${CMAKE_CURRENT_SOURCE_DIR}/SDL3)
+    message(STATUS "SDL3_DIR: ${SDL3_DIR}")
+
+    # Add SDL3 as a subdirectory
+    add_subdirectory(${SDL3_DIR})
 endif ()
 
 if (APPLE)
     add_library(SDL3 SHARED IMPORTED)
     set(SDL3_DIR "${CMAKE_SOURCE_DIR}/External/SDL3")
     set(SDL3_LIB "${CMAKE_BINARY_DIR}/External/SDL3/libSDL3.dylib" CACHE STRING "Path to SDL3 static library") # Adjust if the build path differs
-
     set_target_properties(SDL3 PROPERTIES
             IMPORTED_LOCATION "${SDL3_LIB}"
             INTERFACE_INCLUDE_DIRECTORIES "${SDL3_INCLUDE}"
